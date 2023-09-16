38fddfd01a6d8ea21191a3578a64836510ae8fd3
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-09-16 19:11:13 -0400

    Perfect face recognition feature

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,11 @@
 import asyncio
 import cv2
-import numpy as np
+import imutils
+
 from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
 from viam.components.camera import Camera
-from viam.services.vision import Detection
 from viam.components.base import Base
-from viam.services.vision import VisionClient
-from PIL import Image
 import json
 
 
@@ -20,72 +18,37 @@ async def connect():
     )
     return await RobotClient.at_address("htn-main.3o4e4wpz0f.viam.cloud", opts)
 
-# Check if this works?
 
-
-def leftOrRight(detections, midpoint, frame):
+def leftOrRight(face_cascade, midpoint):
     largest_area = 0
-    largest = Detection()
-    if not detections:
-        print("nothing detected :(")
+    largest = ()
+    image = cv2.imread('img.jpeg')
+    # Convert into grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    if len(faces) == 0:
+        print("nothing detected")
         return -1
-    for d in detections:
-        a = (d.x_max - d.x_min) * (d.y_max-d.y_min)
+
+    # Draw rectangle around the faces
+    for face in faces:
+        (x, y, w, h) = face
+        a = w*h
         if a > largest_area:
-            a = largest_area
-            largest = d
-    if frame is not None:
-        # Convert frame to a format that can be saved
-        file_path = "test_image.jpg"
-        with open(file_path, "wb") as f:
-            f.write(frame.data)
-        image = cv2.imread('test_image.jpg')
-        border_size = 10  # You can adjust this to your desired border width
-        # Color in BGR format (Green in this example)
-        border_color = (0, 255, 0)
-        bordered_image = cv2.copyMakeBorder(
-            image,
-            top=largest.y_max,
-            bottom=largest.y_min,
-            left=largest.x_min,
-            right=largest.x_max,
-            borderType=cv2.BORDER_CONSTANT,
-            value=border_color
-        )
-        cv2.imwrite('border_img.jpg', bordered_image)
+            largest_area = a
+            largest = face
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imwrite('border_img.jpeg', image)
+
+    (x, y, w, h) = largest
+    cx = (x + (x + w))/2
+    if (cx < (midpoint - (midpoint/6))):
+        return 0
+    if (cx > (midpoint + (midpoint/6))):
+        return 2
     else:
-        print("Error cannot capture picture")
-
-    centerX = (largest.x_min + largest.x_max)/2
-    if centerX < (midpoint-midpoint)/6:
-        return 0  # on the left
-    if centerX > (midpoint+midpoint)/6:
-        return 2  # on the right
-    else:
-        return 1  # basically centered
-
-
-async def stop_robot(base):
-    # This is to stop the robot from moving :P
-    await base.stop()
-
-# Not passing in frame just yet but taking it within module
-# Change design as fit later on
-# This will run in a while loop initially? just to make sure we can get the person in frame
-
-
-async def is_person_in_center_frame(camera, detector):
-    frame = await camera.get_image(mime_type="image/jpeg")
-    x, y = frame.size[0], frame.size[1]
-
-    # Crop the image to get only the middle fifth of the top third of the original image
-    cropped_frame = frame.crop((x / 2.5, 0, x / 1.25, y / 3))
-
-    detections = await detector.get_detections(cropped_frame)
-
-    if detections != []:
-        return True
-    return False
+        return 1
 
 
 async def main():
@@ -93,29 +56,29 @@ async def main():
     vel = 500            # go this fast when moving motor
     robot = await connect()
 
-    # peopleClassifier - need to upload this model onto that
-    peopleDetector = VisionClient.from_robot(robot, "person-detection")
-
     roverBase = Base.from_robot(robot, "viam_base")
-    camera = Camera.from_robot(robot=robot, name="cam2")
+    camera = Camera.from_robot(robot=robot, name="cam")
+
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
     print("Robot Connected!")
     print("Resources:")
-    # print(robot.resource_names)
+    print(robot.resource_names)
     # Main loop. Detect the ball, determine if it's on the left or right, and head that way.
     # Repeat this for until we have a good picture
-    peopleDetected = []
-    for i in range(numCycles):
-        detections = await peopleDetector.get_detections_from_camera("cam2")
-        for detection in detections:
-            if (detection.class_name == "0"):
-                peopleDetected.append(detection)
-                print(f'people detected {peopleDetected}')
 
-        # take a picture and send it to ML model for better accurcy
+    for i in range(numCycles):
         frame = await camera.get_image()
         frame1 = await camera.get_image(mime_type="image/jpeg")
-        answer = leftOrRight(peopleDetected, frame1.size[0]/2, frame)
+        if frame is not None:
+            # Convert frame to a format that can be saved
+            file_path = "img.jpeg"
+            with open(file_path, "wb") as f:
+                f.write(frame.data)
+
+        answer = leftOrRight(face_cascade, frame1.size[0]/2)
+
         if answer == 0:
             print("left")
             await roverBase.spin(angle=5, velocity=vel)     # CCW is positive
