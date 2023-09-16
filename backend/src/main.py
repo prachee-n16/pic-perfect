07045ccac59693835c90fbe7e92eefc73e064f5c
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-09-16 17:34:19 -0400

    some trial

--- backend/src/main.py
+++ backend/src/main.py
@@ -22,25 +22,27 @@ async def connect():
 
 # Check if this works?
 
+
 def leftOrRight(detections, midpoint, frame):
-   largest_area = 0
-   largest = Detection()
-   if not detections:
-      print("nothing detected :(")
-      return -1
-   for d in detections:
-      a = (d.x_max - d.x_min) * (d.y_max-d.y_min)
-      if a > largest_area:
-         a = largest_area
-         largest = d
-   if frame is not None:
+    largest_area = 0
+    largest = Detection()
+    if not detections:
+        print("nothing detected :(")
+        return -1
+    for d in detections:
+        a = (d.x_max - d.x_min) * (d.y_max-d.y_min)
+        if a > largest_area:
+            a = largest_area
+            largest = d
+    if frame is not None:
         # Convert frame to a format that can be saved
         file_path = "test_image.jpg"
         with open(file_path, "wb") as f:
             f.write(frame.data)
         image = cv2.imread('test_image.jpg')
         border_size = 10  # You can adjust this to your desired border width
-        border_color = (0, 255, 0)  # Color in BGR format (Green in this example)
+        # Color in BGR format (Green in this example)
+        border_color = (0, 255, 0)
         bordered_image = cv2.copyMakeBorder(
             image,
             top=largest.y_max,
@@ -51,16 +53,17 @@ def leftOrRight(detections, midpoint, frame):
             value=border_color
         )
         cv2.imwrite('border_img.jpg', bordered_image)
-   else:
+    else:
         print("Error cannot capture picture")
 
-   centerX = (largest.x_min + largest.x_max)/2
-   if centerX < (midpoint-midpoint)/6:
-      return 0 # on the left
-   if centerX > (midpoint+midpoint)/6:
-      return 2 # on the right
-   else:
-      return 1  # basically centered
+    centerX = (largest.x_min + largest.x_max)/2
+    if centerX < (midpoint-midpoint)/6:
+        return 0  # on the left
+    if centerX > (midpoint+midpoint)/6:
+        return 2  # on the right
+    else:
+        return 1  # basically centered
+
 
 async def stop_robot(base):
     # This is to stop the robot from moving :P
@@ -95,7 +98,6 @@ async def main():
 
     roverBase = Base.from_robot(robot, "viam_base")
     camera = Camera.from_robot(robot=robot, name="cam2")
-    
 
     print("Robot Connected!")
     print("Resources:")
@@ -106,21 +108,21 @@ async def main():
     for i in range(numCycles):
         detections = await peopleDetector.get_detections_from_camera("cam2")
         for detection in detections:
-            if(detection.class_name == "0"):
+            if (detection.class_name == "0"):
                 peopleDetected.append(detection)
                 print(f'people detected {peopleDetected}')
 
         # take a picture and send it to ML model for better accurcy
         frame = await camera.get_image()
-        frame1= await camera.get_image(mime_type="image/jpeg")
+        frame1 = await camera.get_image(mime_type="image/jpeg")
         answer = leftOrRight(peopleDetected, frame1.size[0]/2, frame)
         if answer == 0:
             print("left")
             await roverBase.spin(angle=5, velocity=vel)     # CCW is positive
-            await roverBase.move_straight(distance=600,velocity=vel)
+            await roverBase.move_straight(distance=600, velocity=vel)
         if answer == 1:
             print("center")
-            await roverBase.move_straight(distance=600,velocity=vel)
+            await roverBase.move_straight(distance=600, velocity=vel)
         if answer == 2:
             print("right")
             await roverBase.spin(angle=-5, velocity=vel)
