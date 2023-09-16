adfd4813a548a5968dceb08172f0c7844eaa7286
Prachee Nanda <p4nanda@uwaterloo.ca>
2023-09-16 11:46:49 -0400

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      hey there
+      <hr className="divider" />
+    </nav>
+  );
 };
 
 export default Navbar;
