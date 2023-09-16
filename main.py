00990370b87e44cfe3808448ab70cb4c0f54552c
Ayush Ganguly <ayushganguly@v1040-wn-rt-a-117-138.campus-dynamic.uwaterloo.ca>
2023-09-16 06:24:32 -0400

    fixed connection issue

--- main.py
+++ main.py
@@ -5,40 +5,36 @@ from viam.rpc.dial import Credentials, DialOptions
 from viam.components.camera import Camera
 import json
 
+
 async def connect():
     with open("key.json", "r") as f:
         payload = json.load(f)["payload"]
-    creds = Credentials(
-        type='robot-location-secret',
-        payload=payload)
+    creds = Credentials(type="robot-location-secret", payload=payload)
     opts = RobotClient.Options(
-        refresh_interval=0,
-        dial_options=DialOptions(credentials=creds)
+        refresh_interval=0, dial_options=DialOptions(credentials=creds)
     )
-    return await RobotClient.at_address('picperfect-main.5sdqzlgfkj.viam.cloud', opts)
+    return await RobotClient.at_address("htn-main.3o4e4wpz0f.viam.cloud", opts)
+
 
 async def main():
     robot = await connect()
 
     print("Robot Connected!")
-    print('Resources:')
+    print("Resources:")
     print(robot.resource_names)
 
     # take a picture and send it to ML model for better accurcy
-    my_camera = Camera.from_robot(robot=robot, name="Picperfect")
+    my_camera = Camera.from_robot(robot=robot, name="cam")
     frame = await my_camera.get_image()
 
     print("picture is ", frame)
 
-
-    #get the parameters for a better picture clicked, so the web
+    # get the parameters for a better picture clicked, so the web
     # cam can click a better one
-
-    
-
 
     # Don't forget to close the robot when you're done!
     await robot.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
